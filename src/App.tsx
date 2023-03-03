3160fad1be82abe497848e22b1cee256254bda86
jasonshu1229 <shuhao1229@163.com>
2023-03-03 11:49:47 +0800

    feat: 项目搭建-路由懒加载的组件分包

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import routes from './router';
-import { useRoutes } from 'react-router-dom';
+import { useRoutes, Link } from 'react-router-dom';
 
 function App() {
-  return <div className="App">{useRoutes(routes)}</div>;
+  return (
+    <div className="App">
+      <div className="header">
+        <Link to="/discover">发现音乐</Link>
+        <Link to="/mine">我的音乐</Link>
+        <Link to="/focus">关注</Link>
+        <Link to="/download">下载客户端</Link>
+      </div>
+      <Suspense fallback="">
+        <div className="main">{useRoutes(routes)}</div>
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { Navigate, RouteObject } from 'react-router-dom';
 
-import Discover from '@/views/discover';
-import Download from '@/views/download';
-import Focus from '@/views/focus';
-import Mine from '@/views/mine';
+// import Discover from '@/views/discover';
+// import Download from '@/views/download';
+// import Focus from '@/views/focus';
+// import Mine from '@/views/mine';
+
+const Discover = lazy(() => import('@/views/discover'));
+const Download = lazy(() => import('@/views/download'));
+const Focus = lazy(() => import('@/views/focus'));
+const Mine = lazy(() => import('@/views/mine'));
 
 const routes: RouteObject[] = [
   {
