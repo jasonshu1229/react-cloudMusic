efab92475e01ea4e74106e50cd209c8f75e8b0a7
jasonshu1229 <shuhao1229@163.com>
2023-03-10 18:27:49 +0800

    feat: AreaHeader的props的类型约束,以及改为传参

--- src/components/area-header-v1/index.tsx
+++ src/components/area-header-v1/index.tsx
@@ -6,15 +6,26 @@ import { HeaderV1Wrapper } from './style';
 
 interface IProps {
   children?: ReactNode;
+  title?: string;
+  keywords?: string[];
+  moreText?: string;
+  moreLink?: string;
 }
 
 const AreaHeaderV1: FC<IProps> = (props) => {
+  const {
+    title = '默认标题',
+    keywords = [],
+    moreText = '更多',
+    moreLink = '/'
+  } = props;
+
   return (
     <HeaderV1Wrapper className="sprite_02">
       <div className="left">
-        <h3 className="title">热门推荐</h3>
+        <h3 className="title">{title}</h3>
         <div className="keywords">
-          {['华语', '流行', '摇滚', '民谣'].map((item) => (
+          {keywords.map((item) => (
             <div className="item" key={item}>
               <span className="link">{item}</span>
               <span className="divider">|</span>
@@ -23,8 +34,8 @@ const AreaHeaderV1: FC<IProps> = (props) => {
         </div>
       </div>
       <div className="right">
-        <Link className="more" to="/">
-          更多
+        <Link className="more" to={moreLink}>
+          {moreText}
         </Link>
         <i className="sprite_02 icon" />
       </div>
--- src/views/discover/c-views/recommend/c-cpns/top-banner/hot-recommend/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/top-banner/hot-recommend/index.tsx
@@ -11,7 +11,12 @@ interface IProps {
 const HotRecommend: FC<IProps> = () => {
   return (
     <RecommendWrapper>
-      <AreaHeaderV1 />
+      <AreaHeaderV1
+        title="热门推荐"
+        keywords={['华语', '流行', '摇滚', '民谣']}
+        moreText="更多"
+        moreLink="/discover/songs"
+      />
     </RecommendWrapper>
   );
 };
